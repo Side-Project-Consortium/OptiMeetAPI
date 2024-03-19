1317739214cb532a5d1341dd64959f66f4216cd7
ThomasD76 <thomas.dieulois@yahoo.fr>
2024-03-19 23:12:36 +0100

    Implémentation de la partie User

---
+++ src/api/controllers/userController.js
@@ -0,0 +1,32 @@
+const User = require("../../models/userModel");
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
--- src/app.js
+++ src/app.js
@@ -1,35 +1,21 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userRoutes = require("./api/routes/userRoutes");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use("/api", userRoutes);
 
 mongoose
   .connect(
     "mongodb+srv://optimeet:xJ2IBCoQZOXqjI1F@cluster0.vnbqxlr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
-const db = mongoose.connection;
-
-// Événement de succès de connexion
-db.on("connected", function () {
-  console.log("Connexion à la base de données MongoDB établie avec succès.");
-});
-
-// Événement d'erreur de connexion
-db.on("error", function (err) {
-  console.log("Erreur de connexion à la base de données MongoDB :", err);
-});
-
-// Événement lorsque la connexion est déconnectée
-db.on("disconnected", function () {
-  console.log("Connexion à la base de données MongoDB déconnectée.");
-});
-
-app.get("/", (req, res) => {
-  res.send("Bienvenue sur l'API Side Project!");
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
