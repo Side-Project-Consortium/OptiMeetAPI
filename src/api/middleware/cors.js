5af3fe72152e5f61e0f6cba64afd00735211a7fc
ThomasD76 <thomas.dieulois@yahoo.fr>
2024-06-01 15:43:21 +0200

    test ajout socket.io

--- src/api/middleware/cors.js
+++ src/api/middleware/cors.js
@@ -1,5 +1,5 @@
 function cors(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // À ajuster selon vos besoins
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
--- src/app.js
+++ src/app.js
@@ -5,15 +5,31 @@ const userRoutes = require("./api/routes/userRoutes");
 const logger = require("./api/middleware/logger");
 const errorHandler = require("./api/middleware/errorHandler");
 const corsMiddleware = require("./api/middleware/cors");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const port = 3001;
 
 app.use(corsMiddleware);
 app.use(logger);
-app.use(errorHandler);
 app.use(bodyParser.json());
 app.use("/api", userRoutes);
+
+// Créer un serveur HTTP
+const server = http.createServer(app);
+console.log("HTTP server created");
+
+// Initialiser Socket.IO avec le serveur HTTP
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+  transports: ["websocket", "polling"],
+});
+console.log("Socket.IO initialized");
 
 mongoose
   .connect(
@@ -22,6 +38,29 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
-app.listen(port, () => {
+// Gestion des connexions Socket.IO
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id);
+
+  // Rejoindre une room spécifique
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room ${room}`);
+  });
+
+  // Gestion des messages
+  socket.on("message", (message) => {
+    console.log("Message received:", message);
+    // Émettre le message à tous les clients dans la même room
+    io.to(message.room).emit("message", message);
+  });
+
+  // Déconnexion de l'utilisateur
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ", socket.id);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
