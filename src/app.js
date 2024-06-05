08a7701fbf8c22c076116704ec4df1cafc4a18e2
ThomasD76 <thomas.dieulois@yahoo.fr>
2024-06-05 17:06:47 +0200

    change start script to host on 0.0.0.0

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ const userRoutes = require("./api/routes/userRoutes");
 const logger = require("./api/middleware/logger");
 const errorHandler = require("./api/middleware/errorHandler");
 const corsMiddleware = require("./api/middleware/cors");
-const { createServer } = require("https");
+const { createServer } = require("http");
 const socketIO = require("./socket");
 
 const app = express();
 const port = 80;
 
 const server = createServer(app);
-console.log("HTTPS server created");
+console.log("HTTP server created");
 
 const socket = socketIO(server);
 console.log("Socket.IO initialized");
